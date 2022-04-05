e1e6ba9866e6831ced1638a156116e0d68cd6277
business <cole@nunya.com>
2022-04-04 22:32:44 -0500

    initial commit

---
+++ src/Cole.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function ColeTest() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Figured out why my minified React Files were not loading properly.
+        </p>
+        <p>
+            NGINX just wasn't working right! Uninstalled and reinstalled and wowiee it worked!
+        </p>
+        <a href="https://www.github.com/colelamers">Github</a>
+        <a href="https://www.Linkedin.com/in/colelamers">Linkedin</a>
+        <a href="https://www.colespottery.com">My pottery</a>
+      </header>
+    </div>
+  );
+}
+
+export default ColeTest;
